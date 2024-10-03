16cbd184eeafa54e1cc53f31bce704220f62b088
Ismayilgit24 <ismayilibrahinzad9@gmail.com>
2024-10-03 23:04:47 +0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿//Task - 1
+
+//int[] array = { 24, 15, 33, 7, 0, 1 };
+
+//int minElement = array[0];
+
+//for (int i=0; i<array.Length; i++)
+//{
+//	 if (array[i] < minElement)
+//	{
+//		minElement = array[i];
+//	}
+//}
+//Console.WriteLine("Min element is " + minElement);
+
+
+
+
+
+//Task - 2
+
+//int[] array = { 0, 1, 2, 5, 6, 7 };
+
+//int sum = array[0];
+
+//for (int i=1; i<array.Length; i++)
+//{
+//	sum += array[i];
+//}
+//Console.WriteLine("Sum of the numbers of the array is " + sum);
+
+
+
+
+
+//Task - 3
+
+//string[] array = { "apple", "bear", "come", "Anna", "eat" };
+
+//for (int i=1; i< array.Length; i++)
+//{
+//	if (array[i].Length == 4) 
+//	{
+//		Console.WriteLine(array[i]);
+         
+//	}
+//}
